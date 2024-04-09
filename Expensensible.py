b2e1ce8e2a8e2e418ab529fa2f68e32d506ee703
Shivansh <shuklashivansh1234@gmail.com>
2024-04-09 23:07:13 +0530

    exp

--- Expensensible.py
+++ Expensensible.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from PIL import Image,ImageTk
+# from PIL import Image,ImageTk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
